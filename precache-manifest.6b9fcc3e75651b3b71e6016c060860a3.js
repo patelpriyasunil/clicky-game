7aae98e47d5a8a287193d21a7631e25d1e7c6bcf
Patelpriyasunil <patel.priyasunil@gmail.com>
2019-04-03 17:48:12 -0400

    Updates

---
+++ precache-manifest.6b9fcc3e75651b3b71e6016c060860a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "769e872a99157c266573e7aa6b98a0a2",
+    "url": "/clicky-game/static/media/pooh2.769e872a.jpg"
+  },
+  {
+    "revision": "0301875fe50bb120fd9a",
+    "url": "/clicky-game/static/css/main.e37c8b8f.chunk.css"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "13b937efbccc1d64987630cae937cc9d",
+    "url": "/clicky-game/static/media/Thumbs.13b937ef.db"
+  },
+  {
+    "revision": "e975dd8c1f79d466a06aaab7edf3e011",
+    "url": "/clicky-game/static/media/pooh1.e975dd8c.jpg"
+  },
+  {
+    "revision": "929f4de84b5d5ac0be92ad8d2665a573",
+    "url": "/clicky-game/static/media/pooh10.929f4de8.jpg"
+  },
+  {
+    "revision": "59ee185c6450738d4b357ebf43ba4725",
+    "url": "/clicky-game/static/media/pooh12.59ee185c.jpg"
+  },
+  {
+    "revision": "0301875fe50bb120fd9a",
+    "url": "/clicky-game/static/js/main.7dcd28c6.chunk.js"
+  },
+  {
+    "revision": "734ee498f030caf13f79808405e089e6",
+    "url": "/clicky-game/static/media/pooh3.734ee498.jpg"
+  },
+  {
+    "revision": "cdb0847e9acaa8abf9d8b4af81d061de",
+    "url": "/clicky-game/static/media/pooh4.cdb0847e.jpg"
+  },
+  {
+    "revision": "83f43a33c3a985c840a4948705554a08",
+    "url": "/clicky-game/static/media/pooh5.83f43a33.jpg"
+  },
+  {
+    "revision": "d7e4585685bf4dc9c78b44dfcec16796",
+    "url": "/clicky-game/static/media/pooh6.d7e45856.jpg"
+  },
+  {
+    "revision": "94c71f8f800404012c20cf1841ba8553",
+    "url": "/clicky-game/static/media/pooh7.94c71f8f.jpg"
+  },
+  {
+    "revision": "22d979e8927fb55ffa42575d32fd97ae",
+    "url": "/clicky-game/static/media/pooh8.22d979e8.jpg"
+  },
+  {
+    "revision": "d3f56fe84140728643c065d077818f20",
+    "url": "/clicky-game/static/media/pooh9.d3f56fe8.jpg"
+  },
+  {
+    "revision": "1309fac8a6d51321d85cb0d89db4d9dd",
+    "url": "/clicky-game/index.html"
+  }
+];
